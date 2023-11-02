cd0ffb5a67eab0fd76b9cf11af0ca3a878848ec6
Sai Vishnu <saivishnu725@gmail.com>
2023-11-02 20:42:32 +0530

    set express

---
+++ index.js
@@ -0,0 +1,30 @@
+//jshint esversion:6
+
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+//body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//path
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// public folder
+app.use(express.static("public"));
+
+// use ejs
+app.set("view engine", "ejs");
+
+// get home page
+app.get("/", function (req, res) {
+  res.render("home", { data: data });
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
