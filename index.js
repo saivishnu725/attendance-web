6724884329c8cc459517a2b5211d7b061febc283
Sai Vishnu <saivishnu725@gmail.com>
2023-11-03 08:46:37 +0530

    using data from temp json source

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 
 import express from "express";
 import bodyParser from "body-parser";
+import fs from "fs";
 
 const app = express();
 
 //body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// public folder
+app.use(express.static("public"));
+
+//data
+const data = JSON.parse(fs.readFileSync("public/data/data.json", "utf8"));
 
 //path
 import { fileURLToPath } from "url";
@@ -14,15 +21,12 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// public folder
-app.use(express.static("public"));
-
 // use ejs
 app.set("view engine", "ejs");
 
 // get home page
 app.get("/", function (req, res) {
-  res.render("home");
+  res.render("home", { data: data });
 });
 
 app.listen(3000, function () {
