9a717cbb234df732a16d8b26f92dd6d9e6f43be8
Sai Vishnu <saivishnu725@gmail.com>
2023-11-06 18:59:27 +0530

    create a connection to a pool and make a query

---
+++ public/js/database.js
@@ -0,0 +1,39 @@
+import mysql from "mysql";
+import { config } from "dotenv";
+config();
+
+//creating a connection pool
+const pool = mysql.createPool({
+  connectionLimit: 100,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// create a connection using the pool
+const getConnection = (callback) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      return callback(err, null);
+    }
+    callback(null, connection);
+  });
+};
+
+// perform a query on the database and return the result
+const query = (sql, params, callback) => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) reject(err);
+      else
+        connection.query(sql, params, (error, results) => {
+          connection.release();
+          if (error) reject(error);
+          else resolve(results);
+        });
+    });
+  });
+};
+
+export { query };
