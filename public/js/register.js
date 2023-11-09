dd15dbb106fe8454d57683de62bf83a2c6d1c833
Sai Vishnu <saivishnu725@gmail.com>
2023-11-09 16:45:10 +0530

    registering works

---
+++ public/js/register.js
@@ -0,0 +1,33 @@
+import { query } from "./database.js";
+import bcrypt from "bcrypt";
+
+const checkIfUserExists = async (username, email) => {
+  const queryResult = await query(
+    "SELECT * FROM Users WHERE Username = ? OR Email = ?",
+    [username, email]
+  );
+  return queryResult.length > 0;
+};
+
+const hashPassword = async (password) => {
+  const saltRounds = 10;
+  return bcrypt.hash(password, saltRounds);
+};
+
+const createUser = async ({
+  username,
+  email,
+  firstName,
+  lastName,
+  password,
+}) => {
+  const hashedPassword = await hashPassword(password);
+  const currentDate = new Date();
+  const queryResult = await query(
+    "INSERT INTO Users (Username, PasswordHash, Email, FirstName, LastName, CreatedAt) VALUES (?, ?, ?, ?, ?, ?)",
+    [username, hashedPassword, email, firstName, lastName, currentDate]
+  );
+  return queryResult;
+};
+
+export { checkIfUserExists, createUser };
