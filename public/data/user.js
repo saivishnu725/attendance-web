5f9d84f4d81cd52c6ffbb8d2ebaa212588aad867
saivishnu725 <saivishnu725@gmail.com>
2023-11-21 18:45:13 +0530

    errur handing (yes. intentional mistake)

--- public/data/user.js
+++ public/data/user.js
@@ -3,46 +3,63 @@ import bcrypt from "bcrypt";
 
 // get userID
 export const getUserID = async (email) => {
-  let conn = await pool.getConnection();
-  const result = await conn.query("SELECT UserID FROM Users WHERE Email = ?", [
-    email,
-  ]);
-  console.log("inside getUserID: ", result);
-  return result[0];
+  try {
+    let conn = await pool.getConnection();
+    const result = await conn.query(
+      "SELECT UserID FROM Users WHERE Email = ?",
+      [email]
+    );
+    console.log("inside getUserID: ", result);
+    return result[0];
+  } catch (error) {
+    console.error("Error getting user ID: ", error);
+  }
 };
 
 // get userName
 export const getUserName = async (userID) => {
-  let conn = await pool.getConnection();
+  try {
+    let conn = await pool.getConnection();
 
-  const result = await conn.query(
-    "SELECT Username FROM Users WHERE UserID = ?",
-    [userID]
-  );
-  return result[0].Username;
+    const result = await conn.query(
+      "SELECT Username FROM Users WHERE UserID = ?",
+      [userID]
+    );
+    return result[0].Username;
+  } catch (error) {
+    console.error("Error getting user name: ", error);
+  }
 };
 
 // getUserData
 export const getUserData = async (userID) => {
-  let conn = await pool.getConnection();
-  console.log("getting user data: ", userID);
-  const result = await conn.query(
-    "SELECT UserID, Username, Email, FirstName, LastName, CreatedAt, UpdatedAt FROM Users WHERE UserID = ?",
-    [userID]
-  );
-  console.log("inside getUserData: ", result);
-  return result[0];
+  try {
+    let conn = await pool.getConnection();
+    console.log("getting user data: ", userID);
+    const result = await conn.query(
+      "SELECT UserID, Username, Email, FirstName, LastName, CreatedAt, UpdatedAt FROM Users WHERE UserID = ?",
+      [userID]
+    );
+    console.log("inside getUserData: ", result);
+    return result[0];
+  } catch (error) {
+    console.error("Error getting user data: ", error);
+  }
 };
 
 // check if user exists using email
 export const checkUserExists = async (email) => {
-  let conn = await pool.getConnection();
-  console.log("checking if user exists");
-  const queryResult = await conn.query(
-    "SELECT * FROM Users WHERE Username = ? OR Email = ?",
-    [email, email]
-  );
-  return queryResult.length > 0;
+  try {
+    let conn = await pool.getConnection();
+    console.log("checking if user exists");
+    const queryResult = await conn.query(
+      "SELECT * FROM Users WHERE Username = ? OR Email = ?",
+      [email, email]
+    );
+    return queryResult.length > 0;
+  } catch (error) {
+    console.error("Error checking if user exists: ", error);
+  }
 };
 
 export const hashPassword = async (password) => {
@@ -58,28 +75,36 @@ export const createUser = async (
   lastName
 ) => {
   console.log("creating user");
-  let conn = await pool.getConnection();
-  const hashedPassword = await hashPassword(password);
-  console.log(password, " + ", hashedPassword);
-  const currentDate = new Date();
-  const queryResult = await conn.query(
-    "INSERT INTO Users (Username, PasswordHash, Email, FirstName, LastName, CreatedAt) VALUES (?, ?, ?, ?, ?, ?)",
-    [username, hashedPassword, email, firstName, lastName, currentDate]
-  );
-  return queryResult;
+  try {
+    let conn = await pool.getConnection();
+    const hashedPassword = await hashPassword(password);
+    console.log(password, " + ", hashedPassword);
+    const currentDate = new Date();
+    const queryResult = await conn.query(
+      "INSERT INTO Users (Username, PasswordHash, Email, FirstName, LastName, CreatedAt) VALUES (?, ?, ?, ?, ?, ?)",
+      [username, hashedPassword, email, firstName, lastName, currentDate]
+    );
+    return queryResult;
+  } catch (error) {
+    console.error("Error creating user: ", error);
+  }
 };
 
 // verify user
 export const verifyUser = async (email, password) => {
-  let conn = await pool.getConnection();
-  const result = await conn.query(
-    "SELECT PasswordHash FROM Users WHERE Email = ? OR Username = ?",
-    [email, email]
-  );
-  console.log("verifyUser: ", result);
-  const hashedPassword = result[0].PasswordHash;
-  console.log("hashedPassword: ", hashedPassword);
-  const match = await bcrypt.compare(password, hashedPassword);
-  console.log("match: ", match);
-  return match;
+  try {
+    let conn = await pool.getConnection();
+    const result = await conn.query(
+      "SELECT PasswordHash FROM Users WHERE Email = ? OR Username = ?",
+      [email, email]
+    );
+    console.log("verifyUser: ", result);
+    const hashedPassword = result[0].PasswordHash;
+    console.log("hashedPassword: ", hashedPassword);
+    const match = await bcrypt.compare(password, hashedPassword);
+    console.log("match: ", match);
+    return match;
+  } catch (error) {
+    console.error("Error verifying user: ", error);
+  }
 };
